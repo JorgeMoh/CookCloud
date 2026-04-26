3cac33dae8a21b9a95245387d031771cd277f448
Jorge Mohigefer <jorgemohigefer@gmail.com>
2026-04-26 23:52:16 +0200

    Buscador de recetas -> completado

--- CookCloudApp/src/main/java/cookcloud/controlador/ExploradorController.java
+++ CookCloudApp/src/main/java/cookcloud/controlador/ExploradorController.java
@@ -4,16 +4,21 @@ import cookcloud.modelo.Receta;
 import cookcloud.modelo.TarjetaReceta;
 import cookcloud.modelo.Usuario;
 import cookcloud.servicios.RecipeService;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.FlowPane;
 
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class ExploradorController {
 
 
     @FXML
     public FlowPane fpRecetas;
+    @FXML
+    public TextField tfBuscador;
 
     private Usuario usuario;
     private GeneralController generalController;
@@ -34,11 +39,11 @@ public class ExploradorController {
      */
     private void mostrarRecetas() {
 
-        // busca en la base de datos las recetas
-        List<Receta> recetasCreadas = recipeService.cargarRecetasPublicas(usuario.getId_usuario());
+        // busca en la base de datos las recetas publicas
+        List<Receta> recetasPublicas = recipeService.cargarRecetasPublicas(usuario.getId_usuario());
 
         // las muestra en el componente personalizado
-        for (Receta receta : recetasCreadas) {
+        for (Receta receta : recetasPublicas) {
 
             TarjetaReceta tarjetaReceta = new TarjetaReceta(receta);
             tarjetaReceta.setOnMouseClicked(e -> {
@@ -49,6 +54,54 @@ public class ExploradorController {
             fpRecetas.getChildren().add(tarjetaReceta);
 
         }
+
+    }
+
+    /**
+     * Metodo que busca las recetas que concuerdan con lo que escribe el usuario
+     */
+    public void buscar() {
+
+        // mientras no este vacío buscara
+        if(!tfBuscador.getText().trim().isEmpty()){
+
+            fpRecetas.getChildren().clear();
+
+            // busca en la base de datos las recetas publicas
+            List<Receta> recetasPublicas = recipeService.cargarRecetasPublicas(usuario.getId_usuario());
+
+            Pattern patron = Pattern.compile(tfBuscador.getText(), Pattern.CASE_INSENSITIVE);
+
+            // las muestra en el componente personalizado que c
+            for (Receta receta : recetasPublicas) {
+
+                // cargamos las recetas que el patron escrito por el usuario
+                if(patron.matcher(receta.getTitulo()).find()){
+
+                    TarjetaReceta tarjetaReceta = new TarjetaReceta(receta);
+                    tarjetaReceta.setOnMouseClicked(e -> {
+                        generalController.cargarVistaRecetaPublica(tarjetaReceta.getReceta(), true);
+                    });
+                    tarjetaReceta.mostrarCreador();
+
+                    fpRecetas.getChildren().add(tarjetaReceta);
+
+                }
+
+            }
+
+        }
+
+    }
+
+    /**
+     * Metodo que limpia el buscador, las recetas mostradas y recarga todas las recetas
+     */
+    public void recargar() {
+
+        tfBuscador.clear();
+        fpRecetas.getChildren().clear();
+        mostrarRecetas();
 
     }
 }
